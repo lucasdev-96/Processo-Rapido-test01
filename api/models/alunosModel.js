b750a6c8a5e74c9bb22b9c1d1464e5ebeb8d5505
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-09-29 20:09:13 -0300

    Criando funcao que adiciona um novo estudante

--- api/models/alunosModel.js
+++ api/models/alunosModel.js
@@ -6,8 +6,17 @@ const arrayOfStudents = path.join(__dirname, '.', 'alunos.json');
 const getStudents = async () => {
   try {
     const students = await fs.readFile(arrayOfStudents, 'utf8');
-    return students;
+    console.log(students);
+    return JSON.parse(students);
   } catch (err) {
     return 'Não foi achado os dados';
   }
+};
+
+const createNewStudent = async (nome, email, dataNascimento, numeroMatricula) => {
+  const students = await getStudents();
+  students.push({
+    nome, email, dataNascimento, numeroMatricula,
+  });
+  await fs.writeFile(arrayOfStudents, JSON.stringify(students));
 };
